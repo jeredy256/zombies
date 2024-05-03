c2d11711da419cf355b44278cfc91a18b197095e
Your Name <you@example.com>
2024-05-02 22:28:41 -0300

    subindo

---
+++ src/componets/App/App.tsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import bo1 from "../../assets/bo1.jfif";
+import bo2 from "../../assets/bo2.jfif";
+import bo3 from "../../assets/bo3.jfif";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Single Page Application</h1>
+      </header>
+      <div className="container">
+        <div className="box">
+          <a href="https://m-lyart.vercel.app/pluto_t5_full_game.torrent">
+            <img src={bo1} alt="Image 1" />
+          </a>
+          <h4>Bo1</h4>
+        </div>
+        <div className="box">
+          <a href="https://m-lyart.vercel.app/pluto_t6_full_game.torrent">
+            <img src={bo2} alt="Image 2" />
+          </a>
+          <h4>bo2</h4>
+        </div>
+        <div className="box">
+          <a href="https://m-lyart.vercel.app/t7_full_game.torrent">
+            <img src={bo3} alt="executavel do bo3" />
+          </a>
+          <h4>bo3</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componets/Routes/index.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import App from "../App/App"
+import Error404 from "../Error404"
+
+
+const Rotas = () => {
+    return(
+        <Routes>
+            <Route  path="/zombies" element={<App/>} />
+            <Route path="*" element={<Error404/>} />
+
+
+        </Routes>
+    )
+}
+
+export default Rotas
